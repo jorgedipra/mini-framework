0593906c7e4ca6900ad842cef6f18ea0289f1dd8
jorgedipra <jorgedipra@gmail.com>
2018-02-06 09:04:21 -0500

    footer maqueta

--- view/partials/Landing__footer_partials.php
+++ view/partials/Landing__footer_partials.php
@@ -1,132 +1,56 @@
-
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">   
-    
     <footer>
-        
-        <div class="footer-top"> 
-           <div class="container">
-             <div class="col-lg-2 col-xs-12 text-center">
-                 <a href="#"><i class="fa fa-facebook fa-2x"></i>Facebook</a>
-             </div>
-              <div class="col-lg-2 col-xs-12 text-center">
-                 <a href="#"><i class="fa fa-twitter fa-2x"></i>Twitter</a>
-             </div>
-              <div class="col-lg-2 col-xs-12 text-center">
-                 <a href="#"><i class="fa fa-flickr fa-2x"></i>Flickr</a>
-             </div>
-              <div class="col-lg-2 col-xs-12 text-center">
-                 <a href="#"><i class="fa fa-tumblr fa-2x"></i>Tumblr</a>
-             </div>
-              <div class="col-lg-2 col-xs-12 text-center">
-                 <a href="#"><i class="fa fa-github fa-2x"></i>Github</a>
-             </div>
-              <div class="col-lg-2 col-xs-12 text-center">
-                 <a href="#"><i class="fa fa-google-plus fa-2x"></i>Google</a>
-             </div>
-           </div> 
-        </div>  
-        
-        <div class="container" style="border-top:1px solid grey;">
-            <div class="row text-center">   
-                <div class="col-lg-6 col-lg-offset-3">
-                      <ul class="menu">
-                                 
-                             <li>
-                                <a href="#">Home</a>
-                              </li>
-                                   
-                              <li>
-                                 <a href="#">About</a>
-                              </li>
-                                   
-                              <li>
-                                <a href="#">Blog</a>
-                              </li>
-                                   
-                              <li>
-                                 <a href="#">Gallery</a>
-                              </li>
-                                   
-                              <li>
-                                <a href="#">Contact</a>
-                             </li>
-                   
-                        </ul>
-                </div>
-            </div>
-        </div>
-        
-    </footer>
-    
-    
-    <div class="copyright">
-     <div class="container">
-       
-         <div class="row text-center">
-            <p>Copyright © 2017 All rights reserved</p>
-         </div>
-         
-       </div>
-    </div>
-    <style>
-        body{
-  font-family: 'Raleway', sans-serif;
-}
-
-footer{
-    background-color:#222222;
-    padding: 60px 0px;
-}
-
-footer ul{
-    list-style:none;
-    margin:0 auto;
-    display:inline-block;
-    padding-top:50px;
-}
-
-footer ul li{
-    float:left;
-}
-
-footer ul li a{
-    color:#b1aca1;
-    padding:20px;
-}
-
-footer ul li a:hover{
-    color:#7dabdb;
-    text-decoration:none;
-}
-
-.footer-top{
-    padding-bottom:50px;
-}
-
-.footer-top .fa{
-    font-size:18px;
-    color: #FFF;
-    padding-right:10px;
-}
-
-.footer-top a{
-    color:#FFF;
-    text-decoration:none;
-}
-
-.footer-top .col-lg-2:hover .fa , .footer-top .col-lg-2:hover a{
-    color:#7dabdb;
-}
-
-.copyright{
-    background-color:#f0f0f0;
-}
-
-.copyright p{
-    font-size:12px;
-    padding:15px 0 10px 0;
-}
-
-
-    </style>
+      <div class="footer-top"> 
+       <div class="container">
+        <ul class="menu"> 
+         <li>
+          <a href="#"><i class="fab fa-facebook fab-2x"></i>Facebook</a>
+        </li> 
+        <li>  
+         <a href="#"><i class="fab fa-twitter fab-2x"></i>Twitter</a>
+       </li>                                   
+       <li>
+        <a href="#"><i class="fab fa-flickr fab-2x"></i>Flickr</a>
+      </li>                                   
+      <li>
+        <a href="#"><i class="fab fa-tumblr fab-2x"></i>Tumblr</a>
+      </li>                                   
+      <li>
+        <a href="#"><i class="fab fa-github fab-2x"></i>Github</a>
+      </li>
+      <li>
+        <a href="#"><i class="fab fa-google-plus fab-2x"></i>Google</a>
+      </li>
+    </ul>
+  </div> 
+</div>  
+<div class="container">
+  <div class="row text-center">   
+    <div class="text-center menu_footer">
+      <ul> 
+       <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+       <a href="#">About</a>
+     </li>
+     <li>
+      <a href="#">Blog</a>
+    </li>
+    <li>
+     <a href="#">Gallery</a>
+   </li>
+   <li>
+    <a href="#">Contact</a>
+  </li>
+</ul>
+</div>
+</div>
+</div>
+</footer>    
+<div class="copyright">
+ <div class="container">
+   <div class="row text-center">
+    <p>Copyright © 2017 All rights reserved</p>
+  </div>
+</div>
+</div>
