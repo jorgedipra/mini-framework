19e3e958fdd726e1ab5389f79e285676b230df48
jorgedipra <jorgedipra@gmail.com>
2018-02-07 14:05:01 -0500

    ajuste de css de error 404

--- view/error__404.php
+++ view/error__404.php
@@ -8,22 +8,22 @@ include 'partials/header_partials.php';
 include 'partials/Landing__header_partials.php';
 #@END::header
 ?>
-<div class="container text-center">
+<div class="container text-center" id="error404">
 	<p>
 		<h2>PÁGINA NO ENCONTRADA</h2>
 	</p>
-	<img src="../storage/public/404/404_1.gif" alt="404">
+	<img id="error404__img" src="../storage/public/404/404_1.gif" alt="404" title="404">
 	<p>
 		<h2>	
 			Vaya, parece que esa página no existe.
 		</h2>
 	</p>
 	<p>
-		Los conejos han estado mordisqueando los cables otra vez ... <br>
-		Tal vez esto ayude a casa:
+		Parece que lo cables tienen un fantasma que los molesta... <br>
+		Tal vez esto te ayude a volver a casa:
 	</p>
-	<a href="/" title="Home">	
-		<p style="width: 100px;height: 100px; border: solid 1px #ccc;border-radius: 100%; margin: 0 auto;font-size: 360%">
+	<a href="/" title="Home" id="error404___btnHome">	
+		<p>
 			<i class="fas fa-home"></i>
 		</p>
 	</a>
